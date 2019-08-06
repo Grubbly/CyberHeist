b250acc31fbfaa8a36796afac0e0ddc764399c50
unknown <orthogonaltrajectory@gmail.com>
2019-08-05 23:33:11 -0400

    Camera rotation value passed as URL param

--- Python/Flask/app.py
+++ Python/Flask/app.py
@@ -1,7 +1,16 @@
-from flask import Flask, request
+from flask import Flask, request, escape
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
     return "<h1> Cyber Heist </h1>"
+
+@app.route("/camera", methods=["GET"])
+def control():
+    cameraRotation = request.args.get('cameraRotation')
+
+    if cameraRotation is not None:
+        return '<h1> Camera rotation set to: {} </h1>'.format(escape(cameraRotation))
+    else:
+        return 'Camera rotation not set'
