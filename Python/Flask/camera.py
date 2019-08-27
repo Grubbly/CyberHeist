bccdf310371ad1842dbcbd02f44a92f470cdfd85
unknown <orthogonaltrajectory@gmail.com>
2019-08-27 01:03:18 -0800

    HTML edits

--- Python/Flask/camera.py
+++ Python/Flask/camera.py
@@ -8,10 +8,9 @@ from http import server
 PAGE="""\
 <html>
 <head>
-<title>Raspberry Pi - Surveillance Camera</title>
+<title>Camera</title>
 </head>
 <body>
-<center><h1>Raspberry Pi - Surveillance Camera</h1></center>
 <center><img src="stream.mjpg" width="640" height="480"></center>
 </body>
 </html>
