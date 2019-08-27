0ff583cc3ae0e7398ec46f7193599467eaa7dd85
unknown <orthogonaltrajectory@gmail.com>
2019-08-27 00:51:33 -0800

    Outside try except for type errors

--- Python/Flask/servo.py
+++ Python/Flask/servo.py
@@ -4,20 +4,23 @@ import time
 signalPin = 14
 
 def moveServo(positionVal):
-    if float(positionVal) > 0:
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(signalPin, GPIO.OUT)
-        servo = GPIO.PWM(signalPin, 50)
+    try:
+        if float(positionVal) > 0:
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(signalPin, GPIO.OUT)
+            servo = GPIO.PWM(signalPin, 50)
 
-        try:
-            servo.start(7.5)
+            try:
+                servo.start(7.5)
 
-            servo.ChangeDutyCycle(float(positionVal))
-            time.sleep(0.5)
+                servo.ChangeDutyCycle(float(positionVal))
+                time.sleep(0.5)
 
-            servo.stop()
-            GPIO.cleanup()
-        except Exception as e:
-            servo.stop()
-            GPIO.cleanup()
-            print(e)
+                servo.stop()
+                GPIO.cleanup()
+            except Exception as ei:
+                servo.stop()
+                GPIO.cleanup()
+                print(ei)
+    except Exception as eo:
+        print(eo)
