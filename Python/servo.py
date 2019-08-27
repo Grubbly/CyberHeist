776bad2f62c42da7ea085464100d725c7611f720
unknown <orthogonaltrajectory@gmail.com>
2019-08-27 00:18:42 -0800

    Servo control test

---
+++ Python/servo.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+import time
+
+signalPin = 14
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(signalPin, GPIO.OUT)
+
+servo = GPIO.PWM(signalPin, 50)
+servo.start(7.5)
+
+servo.ChangeDutyCycle(7.5)
+time.sleep(0.3)
+servo.ChangeDutyCycle(10)
+time.sleep(0.3)
+servo.ChangeDutyCycle(7.5)
+time.sleep(0.3)
+servo.ChangeDutyCycle(10)
+time.sleep(0.3)
