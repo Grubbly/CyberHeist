8e7b80bb68855c739668d1c072a49183edc34fb9
unknown <orthogonaltrajectory@gmail.com>
2019-08-27 00:49:11 -0800

    Error checking for Flask values

--- Python/Flask/servo.py
+++ Python/Flask/servo.py
@@ -4,14 +4,20 @@ import time
 signalPin = 14
 
 def moveServo(positionVal):
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(signalPin, GPIO.OUT)
+    if float(positionVal) > 0:
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(signalPin, GPIO.OUT)
+        servo = GPIO.PWM(signalPin, 50)
 
-    servo = GPIO.PWM(signalPin, 50)
-    servo.start(7.5)
+        try:
+            servo.start(7.5)
 
-    servo.ChangeDutyCycle(float(positionVal))
-    time.sleep(0.5)
+            servo.ChangeDutyCycle(float(positionVal))
+            time.sleep(0.5)
 
-    servo.stop()
-    GPIO.cleanup()
+            servo.stop()
+            GPIO.cleanup()
+        except Exception as e:
+            servo.stop()
+            GPIO.cleanup()
+            print(e)
