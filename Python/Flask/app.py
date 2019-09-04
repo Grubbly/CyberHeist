ab9552f95e17bd1690896c15368b9e4c7d7b4a90
unknown <orthogonaltrajectory@gmail.com>
2019-09-03 23:12:11 -0800

    Password parameter in URL
    Insecure authentication

--- Python/Flask/app.py
+++ Python/Flask/app.py
@@ -10,9 +10,13 @@ def home():
 @app.route("/camera", methods=["GET"])
 def control():
     cameraRotation = request.args.get('cameraRotation')
+    password = request.args.get('password')
 
-    if cameraRotation is not None:
-        moveServo(cameraRotation)
-        return '<h1> Camera rotation set to: {} </h1> <div><iframe src="localhost:8000/index.html"></iframe><div>'.format(escape(cameraRotation))
+    if password == "udonnoodles":
+        if cameraRotation is not None:
+            moveServo(cameraRotation)
+            return '<h1> Camera rotation set to: {} </h1> <div><iframe src="localhost:8000/index.html"></iframe></div>'.format(escape(cameraRotation))
+        else:
+            return '<h1> Camera rotation not set </h1> <div><iframe src="localhost:8000/index.html"></iframe></div>'
     else:
-        return '<h1> Camera rotation not set </h1> <div><iframe src="localhost:8000/index.html"></iframe><div>'
+        return '<h1> Invalid password, the correct password must be provided to control the camera </h1>'
